426b8968ab244a7720175a5bba1407ace98aa2a1
Aaron Kalair <aaronkalair@gmail.com>
2016-04-23 22:51:39 +0100

    feat: Calculate weight of tea excluding pot

--- status_helpers.py
+++ status_helpers.py
@@ -8,23 +8,30 @@ logging.basicConfig(level=logging.DEBUG)
 
 def get_descriptor(tea_status, num_of_cups):
     status = namedtuple("status", ["tea_state", "timestamp", "num_of_cups"])
-    return status(tea_state=tea_status, timestamp=datetime.now(), num_of_cups)
+    return status(
+        tea_state=tea_status, timestamp=datetime.now(), num_cups=num_of_cups)
 
 
-def get_is_teapot_full(new_teapot_weight, teapot_weight, weight_of_tea_in_cup):
-    lower_bound = new_teapot_weight - weight_of_tea_in_cup
-    return lower_bound <= teapot_weight
+def get_is_teapot_full(
+        weight_of_tea_in_new_pot, tea_weight, weight_of_tea_in_cup):
+    lower_bound = weight_of_tea_in_new_pot - weight_of_tea_in_cup
+    return lower_bound <= tea_weight
 
-def get_remaining_cups(teapot_weight, empty_teapot_weight, weight_of_tea_in_cup):
-  remaining_cups = (teapot_weight - empty_teapot_weight) / weight_of_tea_in_cup
-  if remaining_cups >= 0.70:
-    return 1
-  else:
-    return int(remaining_cups)
+
+def get_remaining_cups(
+        teapot_weight, empty_teapot_weight, weight_of_tea_in_cup):
+    remaining_cups = \
+        (teapot_weight - empty_teapot_weight) / weight_of_tea_in_cup
+    if remaining_cups >= 0.70:
+        return 1
+    else:
+        return int(remaining_cups)
+
 
 def get_teapot_status(
-         current_weight, temperature, new_teapot_weight,
-        empty_teapot_weight, cold_teapot_temperature, temperature_rising, weight_of_tea_in_cup):
+        current_weight, temperature, new_teapot_weight,
+        empty_teapot_weight, cold_teapot_temperature, temperature_rising,
+        weight_of_tea_in_cup):
 
     logging.debug("\nTemperature %s\
                    \nWeight %s\
@@ -32,21 +39,25 @@ def get_teapot_status(
                    \nEmpty Teapot Weight %s\
                    \nCold Teapot Temp %s\
                    \nTemperature Rising %s"
-                   % (temperature, weight, new_teapot_weight,
+                   % (temperature, current_weight, new_teapot_weight,
                    empty_teapot_weight,
                    cold_teapot_temperature, temperature_rising)
-                 )
-    num_of_cups = get_remaining_cups(current_weight, empty_teapot_weight, weight_of_tea_in_cup)
+    )
+    num_of_cups = get_remaining_cups(
+        current_weight, empty_teapot_weight, weight_of_tea_in_cup)
 
     if temperature_rising and get_is_teapot_full(
-            new_teapot_weight, weight, 250):
+            new_teapot_weight - empty_teapot_weight,
+            current_weight - empty_teapot_weight,
+            weight_of_tea_in_cup):
         return get_descriptor(TeapotStatuses.NEW_TEAPOT, num_of_cups)
-    if weight > empty_teapot_weight and weight < new_teapot_weight:
+    if current_weight > empty_teapot_weight and \
+            current_weight < new_teapot_weight:
         if temperature > cold_teapot_temperature:
             return get_descriptor(TeapotStatuses.GOOD_TEAPOT, num_of_cups)
         else:
             return get_descriptor(TeapotStatuses.COLD_TEAPOT, num_of_cups)
-    if weight < empty_teapot_weight:
+    if current_weight < empty_teapot_weight:
         return get_descriptor(TeapotStatuses.NO_TEAPOT, num_of_cups)
     if num_of_cups == 0:
         return get_descriptor(TeapotStatuses.EMPTY_TEAPOT, num_of_cups)
