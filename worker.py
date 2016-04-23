b8d5f600b5d5f9cc7da4d8e2dd88fc96b579e8bf
Gareth Brady <gareth.brady92@gmail.com>
2016-04-23 17:02:12 +0100

    WIP

--- worker.py
+++ worker.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from inputs.weight import Weight
 from inputs.temperature import Temperature
 from status_helpers import get_teapot_status
@@ -5,11 +6,18 @@ from status_helpers import get_teapot_status
 weight_sensor = Weight()
 temperature_sensor = Temperature()
 
+cup_of_tea_weight = 250
+
+def get_cups_remaining(current_teapot_weight):
+	return int(current_teapot_weight / cup_of_tea_weight)
+
+
 while True:
     weight = weight_sensor.get_reading()
     temperature = temperature_sensor.get_reading()
     status = get_teapot_status(
-        weight=weight, temperature=temperature, new_teapot_temperature=55,
-        new_teapot_weight=3000, empty_teapot_weight=0,
-        cold_teapot_temperature=20, temperature_rising=temperature_sensor.is_rising())
+        weight=weight, temperature=temperature,
+        new_teapot_weight=3042, empty_teapot_weight=1472,
+        cold_teapot_temperature=40, temperature_rising=temperature_sensor.is_rising())
     print status
+    print get_cups_remaining(weight)
