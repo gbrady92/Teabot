62eea33f8631d5d81b4630f367200640fe3201b8
Gareth Brady <gareth.brady92@gmail.com>
2016-04-24 15:48:19 +0100

    WIP

--- worker.py
+++ worker.py
@@ -16,3 +16,5 @@ while True:
         current_weight=current_weight, temperature=temperature,
         new_teapot_weight=3042, empty_teapot_weight=1472,
         cold_teapot_temperature=40, temperature_rising=temperature_sensor.is_rising(), weight_of_tea_in_cup=weight_of_tea_in_cup)
+
+    print status
