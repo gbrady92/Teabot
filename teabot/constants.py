d62da8216bb7c0855e03cefadbf1001a8421b950
Hazel Wright <hazel@conversocial.com>
2019-05-13 14:30:58 +0100

    chore: noticket - questionable constant simplification

--- teabot/constants.py
+++ teabot/constants.py
@@ -6,6 +6,26 @@ FULL_TEAPOT = "FULL_TEAPOT"
 GOOD_TEAPOT = "GOOD_TEAPOT"
 EMPTY_TEAPOT = "EMPTY_TEAPOT"
 
+# Number of minutes we want to wait before reporting a new teapot to the
+# endpoint. By default we assume that a teapot takes 5 minutes to brew.
+BREW_DELAY_MINUTES = 5
+# How many cups are in a full pot of tea?
+CUPS_IN_A_FULL_TEAPOT = 4
+# Weight of an empty teapot. This was naively calculated by putting the teapot
+# on the scales and seeing what the scales reported it as (then adding 100g
+# as the weight does fluctuate over time).
+EMPTY_TEAPOT_WEIGHT = 1550
+# Used to determine when a new teapot has been created. We assume that if the
+# weight on the scales has gone up and the last teapot was created over 40
+# minutes ago it is a new teapot.
+NEW_TEAPOT_REFRESH_PERIOD = timedelta(minutes=40)
+# How many grams in a teacup-cup if a teacup could gram grams?
+WEIGHT_OF_TEA_IN_CUP = 280
+# Nought.
+ZERO_WEIGHT = 0
+
+ENDPOINT_BASE_URL = environ.get("TEABOT_BASE_URL", "https://teabot.co.uk/")
+
 
 class TeapotStatuses(object):
     """Defines the states the teapot can be in"""
@@ -27,61 +47,25 @@ class Constants(object):
     """Defines environmental constants that influence the state of the
     teapot
     """
-
-    def __init__(self):
-        self.FULL_TEAPOT_WEIGHT = None
-        self.EMPTY_TEAPOT_WEIGHT = None
-        self.WEIGHT_OF_TEA_IN_CUP = None
-        self.ZERO_WEIGHT = None
-        self.BREW_DELAY_MINUTES = None
-        self.ENDPOINT_BASE_URL = None
-        self.NEW_TEAPOT_REFRESH_PERIOD = None
-
-    def get_full_teapot_weight(self):
-        if not self.FULL_TEAPOT_WEIGHT:
-            # TODO Replace with actual database lookup
-            self.FULL_TEAPOT_WEIGHT = 796
-        return self.FULL_TEAPOT_WEIGHT
-
     def get_empty_teapot_weight(self):
-        if not self.EMPTY_TEAPOT_WEIGHT:
-            # TODO Replace with actual database lookup
-            self.EMPTY_TEAPOT_WEIGHT = 58
-        return self.EMPTY_TEAPOT_WEIGHT
+        return EMPTY_TEAPOT_WEIGHT
 
     def get_weight_of_tea_in_cup(self):
-        if not self.WEIGHT_OF_TEA_IN_CUP:
-            # TODO Replace with actual database lookup
-            self.WEIGHT_OF_TEA_IN_CUP = 280
-        return self.WEIGHT_OF_TEA_IN_CUP
+        return WEIGHT_OF_TEA_IN_CUP
 
     def get_zero_weight(self):
-        if not self.ZERO_WEIGHT:
-            # TODO Replace with actual database lookup
-            self.ZERO_WEIGHT = 0
-        return self.ZERO_WEIGHT
+        return ZERO_WEIGHT
 
     def get_brew_delay_minutes(self):
-        if not self.BREW_DELAY_MINUTES:
-            # TODO Replace with actual database lookup
-            self.BREW_DELAY_MINUTES = 5
-        return self.BREW_DELAY_MINUTES
+        return BREW_DELAY_MINUTES
 
     def get_endpoint_base_url(self):
-        if not self.ENDPOINT_BASE_URL:
-            self.ENDPOINT_BASE_URL = environ.get(
-                "TEABOT_BASE_URL", "https://teabot.co.uk/")
-        return self.ENDPOINT_BASE_URL
-
-    def get_weight_of_tea_in_full_teapot(self):
-        return self.get_full_teapot_weight() - self.get_empty_teapot_weight()
+        return ENDPOINT_BASE_URL
 
     def get_lower_bound_for_full_teapot(self):
-        return self.get_weight_of_tea_in_full_teapot() - \
-            (self.get_weight_of_tea_in_cup() / 2)
+        return EMPTY_TEAPOT_WEIGHT + (
+            WEIGHT_OF_TEA_IN_CUP * CUPS_IN_A_FULL_TEAPOT
+        )
 
     def get_new_teapot_refresh_period(self):
-        if not self.NEW_TEAPOT_REFRESH_PERIOD:
-            # TODO Replace with actual database lookup
-            self.NEW_TEAPOT_REFRESH_PERIOD = timedelta(minutes=40)
-        return self.NEW_TEAPOT_REFRESH_PERIOD
+        return NEW_TEAPOT_REFRESH_PERIOD
